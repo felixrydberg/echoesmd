151b04df3b6c7fb0ae8462877f4fb95d9aa2223f
felixrydberg <90419848+felixrydberg@users.noreply.github.com>
2024-07-22 22:55:26 +0200

    Removed bunlock. Added website (#1)

--- packages/config-tailwind/index.js
+++ packages/config-tailwind/index.js
@@ -3,8 +3,8 @@ export default {
   darkMode: ['class'],
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,vue}",
-    '../../packages/**/*.{js,ts,jsx,tsx,vue,css,html}',
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    '../../packages/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
   ],
   theme: {
 
